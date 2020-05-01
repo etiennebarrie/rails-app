19c01d580d7936efd7be53c2bb75c531ddda39c8
Étienne Barrié <etienne.barrie@gmail.com>
2020-05-01 10:04:39 -0400

    Path generation bug?

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'root#index'
+
+  get "path-generation/:thing", to: "root#index", as: :path_generation
+  get "path-generation-splat/*thing", to: "root#index", as: :path_generation_splat
 end
