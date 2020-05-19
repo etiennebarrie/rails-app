fb83c4dff9c22f997456bf3bed9ab4c5a1cde8e6
Étienne Barrié <etienne.barrie@gmail.com>
2020-05-19 17:50:13 -0400

    CurrentAttributes are fiber-local variables

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,18 @@
 class RootController < ApplicationController
+  before_action :set_current
+
   def index
+  end
+
+private
+
+  def set_current
+    Current.random = rand(100)
+  end
+
+  helper_method def enumerator
+    Enumerator.new do |yielder|
+      yielder.yield Current.random
+    end
   end
 end
