7f58b72b289a4f3b32e741009a3e48ded65bb738
Étienne Barrié <etienne.barrie@gmail.com>
2023-01-13 20:02:00 +0100

    $ rails g model User && rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_13_184825) do
+ActiveRecord::Schema.define(version: 2023_01_13_190156) do
 
   create_table "accounts", force: :cascade do |t|
     t.text "blob"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
