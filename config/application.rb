a5e27e0f13c9f9deefa11f154abf44544c035b69
Étienne Barrié <etienne.barrie@gmail.com>
2024-03-13 14:32:43 +0100

    Configure db/migrate path

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RailsApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    paths["db/migrate"] = "db/migrations"
   end
 end
