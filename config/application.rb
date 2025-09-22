2423f4d19a1932536114465d017a434b0938732a
Étienne Barrié <etienne.barrie@gmail.com>
2025-09-22 14:45:56 +0200

    Escape JSON responses like 8.0

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module RailsApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_controller.escape_json_responses = true
   end
 end
