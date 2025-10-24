36dc5e895c41f98004e9acfea571f67e419b90d9
Étienne Barrié <etienne.barrie@gmail.com>
2025-10-24 17:42:19 +0200

    Cougar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
+    "ractor.localhost",
     "example.com",     # Allow requests from example.com
     /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   ]
