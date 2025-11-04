9e22167fe4d14e8fbb56535f73da5808f24140be
Étienne Barrié <etienne.barrie@gmail.com>
2025-11-04 12:54:16 +0100

    $ bin/rails generate maintenance_tasks:task TestTask

---
+++ test/tasks/maintenance/test_task_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Maintenance
+  class TestTaskTest < ActiveSupport::TestCase
+    # test "#process performs a task iteration" do
+    #   Maintenance::TestTask.process(element)
+    # end
+  end
+end
