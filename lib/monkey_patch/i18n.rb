1f9f4ea49bb28490eddd6168649baef2d63d7740
Étienne Barrié <etienne.barrie@gmail.com>
2025-11-13 15:46:23 +0100

    More I18n

--- lib/monkey_patch/i18n.rb
+++ lib/monkey_patch/i18n.rb
@@ -1,4 +1,23 @@
 module I18n
+  def self.fallbacks
+    Ractor.current[:i18n_fallbacks] ||= I18n::Locale::Fallbacks.new
+  end
+
+  module Locale
+    module Tag
+      class << self
+        # Returns the current locale tag implementation. Defaults to +I18n::Locale::Tag::Simple+.
+        def implementation
+          @implementation ||= Simple
+        end
+
+        def implementation=(implementation)
+          @implementation = implementation
+        end
+      end
+    end
+  end
+
   module Base
     def config
       Ractor.current[:i18n_config] ||= I18n::Config.new
